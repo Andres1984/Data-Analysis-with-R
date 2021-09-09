e7bd87c11afed4b29acee02226a46ad742c28a6c
Andres <andres.marfh@gmail.com>
2021-09-09 15:13:20 -0500

    Update Logit.r

--- Códigos/Logit.r
+++ Códigos/Logit.r
@@ -27,3 +27,20 @@ ggplot(a,aes(`Default$student`,a))+
   geom_point()+
   labs(x = "Student",y="Probabilidad")
 newdata <- predict(pd, type = "response", se.fit = TRUE)[-3]
+
+
+pd<- glm( default ~ balance, data=Default, family=binomial)
+y=seq(0,3000,0.25)[-1]
+sup=exp(pd$coefficients[1]+pd$coefficients[2]*y)
+inf=1+exp(pd$coefficients[1]+pd$coefficients[2]*y)
+px=sup/inf
+result=as.data.frame(cbind(y,px))
+colnames(result)=c("Balance","PX")
+plot(result$Balance,result$PX,type="l", main="Probabilidad de Default", xlab="Balance",ylab="P(X)",ylim = c(0,1))
+abline(h=c(0,1))
+
+
+ggplot(result,aes(Balance,PX))+
+  geom_line()+
+  geom_abline(result,aes(Balance[6000], px[6000]))+
+  labs(x = "Balance",y="Probabilidad")
